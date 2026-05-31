d65b479e8a9c340503872cec057ac499cd0f51be
Nitesh Singh <your_github_email@example.com>
2026-05-31 07:32:49 +0530

    UI: Activate footer feature links and remove Collect Emails

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -43,12 +43,11 @@ export default function Footer() {
 
 
         <div className="footer-col">
-          <h3>Features <span className="soon-badge">Soon</span></h3>
+          <h3>Features</h3>
           <ul>
-            <li><a href="#" className="disabled-link">Comment-to-DM</a></li>
-            <li><a href="#" className="disabled-link">Grow your Followers</a></li>
-            <li><a href="#" className="disabled-link">Never Miss a Comment</a></li>
-            <li><Link to="/forms">Collect Emails</Link></li>
+            <li><Link to="/hub">Comment-to-DM</Link></li>
+            <li><Link to="/hub">Grow your Followers</Link></li>
+            <li><Link to="/hub">Never Miss a Comment</Link></li>
           </ul>
         </div>
 
