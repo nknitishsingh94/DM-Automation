69144230804f796ce28221c5b226ac38e21aaf4c
Nitesh Singh <your_github_email@example.com>
2026-06-09 23:03:53 +0530

    chore: clean up diagnostic scripts

--- check_row.mjs
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { supabase } from './server/utils/supabase.js';
-
-async function checkRow() {
-  const { data } = await supabase.from('scheduled_posts').select('*').eq('id', '8e9b5da1-69f0-4c35-98b0-2257008a55d9');
-  console.log(data);
-  process.exit(0);
-}
-checkRow();
